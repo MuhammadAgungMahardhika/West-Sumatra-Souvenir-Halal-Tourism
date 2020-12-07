ef9cd4667e9428e4958ea27e584b234882b37899
ringgoagsya <63402411+ringgoagsya@users.noreply.github.com>
2020-12-07 17:19:13 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "localhost";
-	$user = "postgres";
-	$pass = "root";
-	$port = "5432";
-	$dbname = "souv";
+	$host = "ec2-34-202-65-210.compute-1.amazonaws.com";
+$dbname="d8e81d9b1vssg0";
+$user="xgyvwwmmfywbcj";
+$port="5432";
+$pass="e2b76172529e80a65bb47ab7849774d499ef521cc9d09431ab69669604b56885";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 ?>
